3990bc28e1395d4b41b68e380ab2a63f13426879
shahrour <ahmadshahrour951@gmail.com>
2020-12-08 04:42:21 +0400

    Update residence.py

--- residence.py
+++ residence.py
@@ -1,15 +1,29 @@
 class Residence():
-    def __init__(self, name, address, floors=[]):
+    def __init__(self, name, address, floors=None):
         self.name = name
         self.address = address
-        self.floors = floors
+        self.floors = floors or []
 
-    def add_floors(self,  floors):
-      for floor in floors:
-          self.floors.append(floor)
+    def get_floors(self):
+      return self.floors
+
+    def add_floor(self,  floor):
+        self.floors.append(floor)
 
     def remove_floor(self, floor):
-      try:
-        self.floors.remove(floor)
-      except ValueError:
-        print(f"{floor.name} does not exist in {self.name}")
+        try:
+            self.floors.remove(floor)
+        except ValueError:
+            print(f"{floor.name} does not exist in {self.name}")
+    
+    def __repr__(self):
+      floor_names = []
+
+      for floor in self.floors:
+        floor_names.append(floor.name)
+
+      return f"""
+      Residence: {self.name}
+      Address: {self.address}
+      Floors: {floor_names}
+      """
