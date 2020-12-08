65700b4e14adf69485585c271fc5a1982b863c90
shahrour <ahmadshahrour951@gmail.com>
2020-12-08 04:42:23 +0400

    Update institution.py

--- institution.py
+++ institution.py
@@ -1,14 +1,26 @@
 class Institution():
-    def __init__(self, name, residences=[]):
+    def __init__(self, name, residences=None):
         self.name = name
-        self.residences = residences
+        self.residences = residences or []
     
-    def add_residences(self, residence):
-          self.residence.append(residence)
-    
+    def get_residences(self):
+      return self.residences
+
+    def add_residence(self, residence):
+      self.residences.append(residence)
+
     def remove_residence(self, residence):
       try:
         self.residences.remove(residence)
       except ValueError:
         print(f"{residence.name} does not exist in {self.name}")
+    
+    def __repr__(self):
+        residence_names = []
+        for residence in self.residences:
+          residence_names.append(residence.name)
+        return f"""
+        Institution: {self.name}
+        Residences: {residence_names}
+        """
 
