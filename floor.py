9b7d52e0a9084c5d8307c9a064eed73bdb23d098
shahrour <ahmadshahrour951@gmail.com>
2020-12-08 04:42:15 +0400

    Update floor.py

--- floor.py
+++ floor.py
@@ -1,16 +1,29 @@
 class Floor():
-    # This is a component of the Residence and Institution classes
-    def __init__(self, name, students):
+    def __init__(self, name, students=None):
         self.name = name
-        self.students = students
-
-    def add_students(self, students):
-      for student in students:
-        self.students.append(student)
+        self.students = students or []
     
-    def remove_student(self, student):
-      try:
-        self.students.remove(student)
-      except ValueError:
-        print(f"{student.name} does not exist in {self.name}")
+    def get_students(self):
+      return self.students
 
+    def add_student(self, student):
+        self.students.append(student)
+        # print(self.name)
+        # print(self.students)
+
+    def remove_student(self, student):
+        try:
+            self.students.remove(student)
+        except ValueError:
+            print(f"{student.name} does not exist in {self.name}")
+    
+    def __repr__(self):
+      student_names = []
+
+      for student in self.students:
+        student_names.append(student.name)
+
+      return f"""
+      Floor: {self.name}
+      Students: {student_names}
+      """
